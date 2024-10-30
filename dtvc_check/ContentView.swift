fe59b5a72428e22cc6df3f4a9aca8dcef055e2a9
md-redwanul-haque <redwan229900@gmail.com>
2024-10-30 15:18:17 +0600

    state Object and environmentObject done

--- dtvc_check/ContentView.swift
+++ dtvc_check/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var testBool: Bool = false
     @State var countVal: Int = 0
     @ObservedObject var CounterMOdelObject = CounterModel()
+    @ObservedObject var CounterDataModelObject = counterDataModel()
     
     var body: some View {
         Toggle(isOn: $testBool, label: {
@@ -19,6 +20,14 @@ struct ContentView: View {
         .padding()
         Text("Parent Count: \(countVal)")
         ChildView(argVal: $countVal, argVal2: $CounterMOdelObject.testInt)
+        
+        Button("model2Button"){
+            CounterMOdelObject.testInt += 1
+        }
+        Text("Model Data Count: \(CounterMOdelObject.testInt)")
+        TimerView()
+        
+        SuperVIew(counter: CounterDataModelObject)
 //        VStack{
 //            Button("Press Here"){
 //             countVal += 1
@@ -56,6 +65,8 @@ struct ChildView: View{
             argVal2 += 1
         }
         Text("Child Model Data Count: \(argVal2)")
+        
+        
     }
     
     
@@ -68,8 +79,54 @@ struct ChildView: View{
 
 
 class CounterModel: ObservableObject {
-     
     @Published var testInt: Int = 0
+}
+
+
+class counterDataModel: ObservableObject {
+    @Published var testValue: Int = 0
+}
+
+class TimerModel: ObservableObject{
     
+    @Published var timeElipsed: Int = 0
+    
+    
+}
+
+struct SuperVIew: View{
+    @StateObject var counter: counterDataModel
+    var body: some View {
+        SubView().environmentObject(counter)
+       }
+    
+}
+
+struct SubView: View{
+    @EnvironmentObject var counter: counterDataModel
+    var body: some View {
+        VStack{
+            Spacer()
+            Text("Global Count: \(counter.testValue)")
+            Button("Increase Global Val"){
+                counter.testValue += 1
+                
+            }
+            
+        }
+       }
+}
+
+struct TimerView: View{
+    @StateObject var timer = TimerModel()
+    
+    var body: some View{
+        
+        Text("TIme Elapsed: \(timer.timeElipsed)")
+        Button("Increment Time"){
+            timer.timeElipsed += 1
+        }
+        
+    }
     
 }
