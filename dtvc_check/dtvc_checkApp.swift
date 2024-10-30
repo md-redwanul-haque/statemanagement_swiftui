5efd02b7b9fcc317a5c8eb6bafa8f689b4616e83
md-redwanul-haque <redwan229900@gmail.com>
2024-10-30 09:40:20 +0600

    Initial Commit

---
+++ dtvc_check/dtvc_checkApp.swift
@@ -0,0 +1,17 @@
+//
+//  dtvc_checkApp.swift
+//  dtvc_check
+//
+//  Created by Redwan on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct dtvc_checkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
